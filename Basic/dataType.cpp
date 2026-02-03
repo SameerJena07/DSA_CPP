63adff3ba55a93c378d7675fc4dce78fdf7ea0ab
Sameer2005-dot <jena80225@gmail.com>
2026-02-03 22:49:21 +0530

    Added RelationalOperator

--- Basic/dataType.cpp
+++ Basic/dataType.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
 
+/*
     int a = 123;
     cout << a << endl;
 
@@ -21,5 +22,50 @@ int main(){
 
     int size = sizeof(a);
     cout << "size of a is: "<< size << endl;
+*/
+
+/*
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+*/
+
+/*
+    unsigned int a = -123;
+    cout << a << endl;
+*/
+
+/*
+    int a = 2.0/5;
+    cout << a << endl;
+
+    cout << 2.0/5 << endl;
+*/
+
+    int a = 3;
+    int b = 4;
+
+    bool first = (a == b);
+    cout << first << endl;
+
+    bool second = (a > b);
+    cout << second << endl;
+
+    bool third = (a < b);
+    cout << third << endl;
+
+    bool fourth = (a != b);
+    cout << fourth << endl;
+
+    bool fifth = (a >= b);
+    cout << fifth << endl;
+
+    bool sixth = (a <= b);
+    cout << sixth << endl;
 }
 
