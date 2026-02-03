39b470adff8beb59c0262bf37bf70fded6feeb4d
Sameer2005-dot <jena80225@gmail.com>
2026-02-03 22:12:24 +0530

    Added dataType

---
+++ Basic/dataType.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 123;
+    cout << a << endl;
+
+    char b = 's';
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.23;
+    cout << f << endl;
+
+    double d = 1.325;
+    cout << d << endl;
+
+
+    int size = sizeof(a);
+    cout << "size of a is: "<< size << endl;
+}
+
