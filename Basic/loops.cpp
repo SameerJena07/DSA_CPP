ffcf63fca4efe92198dc8d789d9c46ce995fb1c4
Sameer2005-dot <jena80225@gmail.com>
2026-02-04 23:19:43 +0530

    Added one problem

--- Basic/loops.cpp
+++ Basic/loops.cpp
@@ -17,13 +17,59 @@ int main(){
 
 // OR //
 
-    int n;
-    cout <<"Enter a number: ";
-    cin>>n;
+    // int n;
+    // cout <<"Enter a number: ";
+    // cin>>n;
 
+    // int i = 1;
+    // while (i<=n){
+    //     cout<<i<<" ";
+    //     i++;
+    // }
+
+
+
+
+    //Find sum of number 1 to n//
+/*
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    int sum = 0;
     int i = 1;
-    while (i<=n){
-        cout<<i<<" ";
+    while (i <= n){
+        sum = sum+i;
         i++;
     }
+    cout<<"The total sum is: "<<sum;
+*/
+
+
+
+    // ** Sum of all even number ** //
+ /*
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    int sum = 0;
+    int i = 1;
+
+    while (i <= n){
+        if (i % 2 == 0){
+            sum = sum + i;
+            i++;
+        } else {
+            i++;
+        }
+    }
+    cout<<"The total even sum is: "<<sum;
+*/
+
+
+
+
+
+
+
 }
