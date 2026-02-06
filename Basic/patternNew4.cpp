0c5d412d903a980bfdf32897a7d4c3deb47e8593
Sameer2005-dot <jena80225@gmail.com>
2026-02-06 23:08:49 +0530

    Added newOne

--- Basic/patternNew4.cpp
+++ Basic/patternNew4.cpp
@@ -7,14 +7,30 @@ int main(){
     cout<<"Enter a number: ";
     cin>>n;
 
-    for (int i = 1; i <= n; i++){
-        char ch = 'A';
-        for (int j=1; j<=i; j++){
-            char m = ch;
-            cout<<m;
+    // for (int i = 1; i <= n; i++){
+    //     char ch = 'A';
+    //     for (int j=1; j<=i; j++){
+    //         char m = ch;
+    //         cout<<m;
 
-            ch = ch + 1;
+    //         ch = ch + 1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    int i = 1;
+    char ch = 'A';
+
+    while (i<=n){
+        int j = 1;
+        while(j<=i){
+            cout<<ch;
+
+            
+            j++;
         }
         cout<<endl;
+        ch++;
+        i++;
     }
 }
