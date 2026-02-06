ae365379e64e44f5b81d652404449a61871de252
Sameer2005-dot <jena80225@gmail.com>
2026-02-06 19:42:52 +0530

    Added another pattern

--- Basic/patternOne.cpp
+++ Basic/patternOne.cpp
@@ -117,7 +117,7 @@ int main(){
 
 
     //* Print another triangle with equal row number value*//
-
+/*
     int n;
     cout<<"Enter a number: ";
     cin>>n;
@@ -135,6 +135,91 @@ int main(){
         
         row = row+ 1;
     }
+*/
+
+
+    //* Added another pattern *//
+/*
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int row = 1;
+    int count = 1;
+
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= row)
+        {
+            cout<<count<<" ";
+            count = count + 1;
+
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+*/
+
+
+    // Print a triangle like 1, 23, 345, 4567 //
+/*
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int row = 1;
+    while (row <= n){
+        int col = 1;
+        int value = row;
+        while (col <= row){
+        cout << value <<" ";
+        value = value + 1;
+
+        col = col + 1;
+        }
+    cout<< endl;
+
+    row = row + 1;
+    }
+*/
+
+/*
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
     
+    for (int row =1; row <= n; row++){
+        int value = row;
+        for (int col = 1; col <= row; col++){
+            cout<<value<<" ";
+            value++;
+        }
+        cout<<endl;
+    }
+*/
+
+
+
+    // print 1, 21, 321, 4321 .....//
+
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int i = 1;
+    while (i <= n) {
+        int j = 1;
+        while (j <= i){
+            cout<< (i-j+1) <<" ";
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i + 1;
+    }
+
+
+
 
 }
