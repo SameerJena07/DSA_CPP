33cc2b817439e8949d99e422d098839ba2ff1429
Sameer2005-dot <jena80225@gmail.com>
2026-02-09 01:48:46 +0530

    Added Problems

---
+++ Basic/BitwiseOperators/Questions.cpp
@@ -0,0 +1,35 @@
+// Question 1
+/*
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b = 1;
+
+    a = 10;
+    if (++a)
+        cout << b;
+    else 
+        cout << ++b;
+}
+
+    //Output: 1;
+*/    
+
+
+// Question 2
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 1;
+    int b = 2;
+
+    if (a-- > 0 && ++b > 2){
+        cout << "Stage1 - Inside If ";
+    } else {
+        cout << "Stage2 - Inside else ";
+    }
+    cout << a << " " << b << endl;
+}
