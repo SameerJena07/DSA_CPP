bd2b714e078b9401830004df872da0c4d34c81da
Sameer2005-dot <jena80225@gmail.com>
2026-02-11 23:09:15 +0530

    Added DecimalTOBinary

---
+++ Basic/BitwiseOperators/DecToBin.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    int ans = 0;
+    int i = 1;
+    while (n != 0) {
+
+        int bit = n & 1;
+
+        ans = (bit * i) + ans;
+
+        n = n >> 1;
+
+        i = i * 10;
+    }
+    cout << "Answer is: " << ans << endl;
+}
